4bb18f194ebe5ae02bf1ffa24cb9c3816fb52aa2
Yisus <jesusz.unam@gmail.com>
2019-04-24 17:34:04 -0500

    Modificación a listados candidatos.

--- track.sql
+++ track.sql
@@ -209,8 +209,6 @@ insert into sistema.modulos (clave_modulo, nombre,url, activo, modulo_padre_clav
 insert into sistema.roles_modulos (clave_modulo, clave_rol, activo) values
 ('CAINFO_CURSO','INV',true);
 
-insert into idiomas.traduccion values('er_mensaje','lbl','revisados','{"es":"Ver","en":"Ver"}');
-
 insert into sistema.modulos (clave_modulo, nombre,url, activo, modulo_padre_clave, orden, clave_configurador_modulo) values 
 ('SAVE_VALCUR',	'{"es":"Carga informacion de curso","en":""}',	'/revision/guarda_validacion_cursos',true,'M2nxOiMxoD',1,'ACCION'),
 ('SAVE_VALDOC',	'{"es":"Carga informacion de documentos","en":""}',	'/revision/guarda_validacion_documentos',true,'M2nxOiMxoD',1,'ACCION'),
@@ -226,3 +224,8 @@ insert into sistema.roles_modulos (clave_modulo, clave_rol, activo) values
 insert into idiomas.traduccion values('er_mensaje','lbl','candidatos','{"es":"No existen datos relacionados.","en":"No existen datos relacionados."}');
 insert into idiomas.traduccion values('er_mensaje','lbl','revisados','{"es":"No existen datos relacionados.","en":"No existen datos relacionados."}');
 
+
+insert into sistema.modulos (clave_modulo, nombre,url, activo, modulo_padre_clave, orden, clave_configurador_modulo) values 
+('GET_DETALLE_CURSO',	'{"es":"Carga informacion de curso","en":""}',	'/registro/get_detalle_curso',true,'M2nxOiMxoD',1,'ACCION');
+insert into sistema.roles_modulos (clave_modulo, clave_rol, activo) values
+('GET_DETALLE_CURSO','INV',true);
