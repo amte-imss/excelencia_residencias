e45526343dda646d901f88b94c729565c4dba2f7
leas88 <cenitluis.pumas@gmail.com>
2019-04-24 23:14:59 -0500

    ssss

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -348,11 +348,11 @@ $config['form_registro_usuario'] = array(
         'label' => 'Confirmación contraseña',
         'rules' => 'trim|required|matches[reg_password]'
     ),
-    /*array(
+    array(
         'field' => 'reg_captcha',
         'label' => 'Captcha',
         'rules' => 'required|check_captcha'
-    )*/
+    )
 );
 $config['form_registro_usuario_internos'] = array(
     array(
